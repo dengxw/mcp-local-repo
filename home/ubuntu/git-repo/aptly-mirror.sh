e9eb20badccbba3e5b351f2784383f7ecb480fb3
dengxw <deng_xiwen@hotmail.com>
2017-09-03 11:20:17 +0800

    Update aptly-mirror.sh

--- home/ubuntu/git-repo/aptly-mirror.sh
+++ home/ubuntu/git-repo/aptly-mirror.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
-#
-# Copyright 2014 Hewlett-Packard Development Company, L.P.
-# All Rights Reserved.
-# Authored by Yazz D. Atlas <yazz.atlas@hp.com>
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-#
+
 # Creating a tempfile directory to use later maybe
 tempdir=$(mktemp -d )  || { echo "Failed to create temp file"; exit 1; }
 line="---------------------------------------------------"
 
-# If I need tempfiles lets do it cleanly 
+# If I need tempfiles lets do it cleanly
 function cleanup {
   echo ${line}
-#  cat ${tempdir}/*
-  rm -rf ${tempdir} 
+  rm -rf ${tempdir}
   echo "Removed ${tempdir}"
   exit
 }
@@ -36,18 +19,13 @@ function fail {
     echo "the command executing at the time of the error was"
     echo "$BASH_COMMAND"
     echo "on line ${BASH_LINENO[0]}"
-    # do some error handling, cleanup, logging, notification
-    # $BASH_COMMAND contains the command that was being executed at the time of the trap
-    # ${BASH_LINENO[0]} contains the line number in the script of that command
-    # exit the script or return to try again, etc.
     cleanup
     exit $errcode  # or use some other value or do return instead
 }
 
-# Catch the crtl-c and others nicely 
-#trap cleanup EXIT SIGHUP SIGINT SIGTERM
-#trap fail ERR
-# 
+# Catch the crtl-c and others nicely
+trap cleanup EXIT SIGHUP SIGINT SIGTERM
+trap fail ERR
 
 # Wanted to output a nicer message while I debug things.
 print(){
@@ -55,12 +33,10 @@ print(){
  echo "${line}"
 }
 
-# I needed this to get snapshots to work. Its not ideal to hardcode this
-# but it is what it is right now.
-components="main,contrib"
 
-# Using an associative array 
+# Using an associative array
 declare -A MIRROR
+
 
 MIRROR[testing-extra]="http://apt.mirantis.com/xenial testing extra"
 MIRROR[testing-salt]="http://apt.mirantis.com/xenial testing salt"
@@ -99,18 +75,18 @@ create_mirror(){
   # Timestamp
   # Just wanting a list of mirrors before I started
   start_time
-  aptly mirror list > ${tempdir}/mirror-list  
-  
-  for mirror in "${!MIRROR[@]}"; do 
-    if ! aptly mirror show $mirror > /dev/null ; then 
+  aptly mirror list > ${tempdir}/mirror-list
+
+  for mirror in "${!MIRROR[@]}"; do
+    if ! aptly mirror show $mirror > /dev/null ; then
       print "aptly mirror create $mirror ${MIRROR[$mirror]}"
-      aptly mirror create $mirror ${MIRROR[$mirror]} 
+      aptly mirror create $mirror ${MIRROR[$mirror]}
     fi
   done
   end_time
 }
 
-# Updating the mirrors and creates a snapshot for the day. Then publishes the mirror. 
+# Updating the mirrors and creates a snapshot for the day. Then publishes the mirror.
 # The publishing off a new snapshot requires dropping the already published right now.
 update_mirror(){
   start_time
@@ -120,7 +96,7 @@ update_mirror(){
 
 create_snapshot(){
   start_time
-  for mirror in "${!MIRROR[@]}"; do 
+  for mirror in "${!MIRROR[@]}"; do
     if ! aptly snapshot show ${mirror}-$(date +%Y%m%d) > /dev/null ; then
       print "aptly snapshot create ${mirror}-$(date +%Y%m%d) from $mirror"
       aptly snapshot create ${mirror}-$(date +%Y%m%d) from mirror $mirror
@@ -138,29 +114,8 @@ publish(){
       component=$(aptly mirror show ${mirror} |grep Components |head -1 |cut -d ' ' -f 2)
       distribution=$(aptly mirror show ${mirror} |grep Distribution |head -1 |cut -d ' ' -f 2)
       aptly publish $1 -architectures='"${arch}"' -component="${component}" -distribution="${distribution}" ${snapshot} ${mirror}
-      #aptly publish $1  -component=${component} -distribution=${distribution} ${snapshot}
     done
 }
-#publish(){
-#  start_time
-#  for mirror in "${!MIRROR[@]}"; do 
-#    echo ${mirror}  | sed -e s/_.*$//g >> ${tempdir}/distribution
-#  done
-#  for distribution in $(cat ${tempdir}/distribution | sort -u) ; do  
-#    snapshot_list=$(aptly snapshot list -raw | grep ${distribution}| grep -v updates | tr "\\n" " " )
-#    snapshot_list_updates=$(aptly snapshot list -raw | grep ${distribution} | grep updates | tr "\\n" " " )
-
-#    echo "------publish $snapshot_list"
-#    echo "------ publish $snapshot_list_updates"
-#    echo "------ publish distribution ${distribution}"
-#    #print "aptly publish $1 -component="main,contrib" -distribution=${distribution} ${snapshot_list} atg"
-#    aptly publish $1 -component="${components}" -distribution="${distribution}" ${snapshot_list} atg
-#    #print "aptly publish $1 -component="main,contrib" -distribution=${distribution}-updates ${snapshot_list_updates} atg"
-#    aptly publish $1 -component="${components}" -distribution="${distribution}"-updates ${snapshot_list_updates} atg
-#  done
-#  rm -f ${tempdir}/distribution
-##  end_time
-#}
 
 
 # Do things here
@@ -172,10 +127,5 @@ publish snapshot
 
 aptly publish list
 
-# Clean up the aptly db of dangling references and packages nolonger used in the repos or 
-# snapshots
-#aptly db cleanup
-#aptly graph
-#mv /tmp/aptly-graph* /aptly/public/aptly-graph.png
-
-# vim: tabstop=2:softtabstop=2:shiftwidth=2:noexpandtab
+# Clean up the aptly db of dangling references and packages nolonger used in the repos or
+aptly db cleanup
